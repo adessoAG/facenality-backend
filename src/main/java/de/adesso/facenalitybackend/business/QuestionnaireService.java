49f3c315222fd4d8daf17de20e20da720f479c24
Sergej Grilborzer <sergej1995@gmail.com>
2018-11-16 12:40:06 +0100

    Add correct-test-avg method

--- src/main/java/de/adesso/facenalitybackend/business/QuestionnaireService.java
+++ src/main/java/de/adesso/facenalitybackend/business/QuestionnaireService.java
@@ -47,4 +47,33 @@ public class QuestionnaireService {
 
         return avgList;
     }
+
+    public void correctQuestionnaire() {
+       List<Questionnaire> questionnaires = questionnaireRepository.findAll();
+        long lastInvalidId = 31;
+
+
+       questionnaires.forEach(questionnaire -> {
+           if (questionnaire.getId() <= lastInvalidId) {
+               Map<String, Integer> correctCattels16Values = questionnaire.getCattells16Questions();
+
+               correctCattels16Values.forEach((key, value) -> {
+                   switch(value) {
+                       case (1): correctCattels16Values.put(key, 5);
+                                break;
+                       case (2): correctCattels16Values.put(key, 4);
+                                break;
+                       case (4): correctCattels16Values.put(key, 2);
+                                break;
+                       case (5): correctCattels16Values.put(key, 1);
+                                break;
+                   }
+               });
+
+               questionnaire.setCattells16Questions(correctCattels16Values);
+           }
+       });
+
+       questionnaireRepository.saveAll(questionnaires);
+    }
 }
